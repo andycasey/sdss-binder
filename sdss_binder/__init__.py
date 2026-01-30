1852c51485e2177f4998207e0f619954a53d7fd0
Andy Casey <andycasey@gmail.com>
2026-01-30 14:29:41 +1100

    try path

--- sdss_binder/__init__.py
+++ sdss_binder/__init__.py
@@ -4,12 +4,12 @@ import os
 
 def setup():
 	def mappath(path):
-		return 'introduction.ipynb'
+		return '/introduction.ipynb'
 
 	return {
 		'mappath': mappath,
 		'launcher_entry': {
 			'title': 'SDSS and BinderHub',
-			'icon_path': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'sdss.ico')
+			'icon_path': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'sdss.svg')
 		},
 	}
